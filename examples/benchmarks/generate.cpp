4c59fcc400e5bcd5a933f7656920bd1c493f69bd
qoofyk_stampede2 <qoofyk@gmail.com>
2020-05-28 18:20:34 -0500

    generate parameter

---
+++ examples/benchmarks/generate.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  int cores = atoi(argv[1]);
+  int times = atoi(argv[2]);
+  for (int j = 4; j <= times; j++) {
+    int n = cores * j;
+    cout << j << " " << n << '\n';
+    cout << "1 ";
+    for (int i = 2; i <= cores; ++i)
+      if (n % i == 0 && (i & 1) == 0) {
+        cout << i << " ";
+      }
+    cout << '\n';
+  }
+
+    return 0;
+}
