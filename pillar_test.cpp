0d956984b4203d35fee186f60a117b90256c4ddb
Yuankun Fu <fu121@purdue.edu>
2020-07-06 17:22:13 -0400

    Update pillar_test.cpp

--- pillar_test.cpp
+++ pillar_test.cpp
@@ -25,17 +25,19 @@ using namespace std;
 // #define ykTile 32
 
 typedef int plint;
-plint*** grid;
-plint* rawData;
+plint*** palabos_grid;
+plint* palabos_rawData;
+
+// Using Pillar memory layout
+plint*** pillar_grid;
+plint* pillar_rawData;
 plint newNx;
-plint ykTile; // actual memory lay out on Long & Width
-plint ykBlockH; // actual memory lay out on Height
-plint NzTiles;
-plint NyTiles;
-plint allocNx;
-plint allocNy;
-plint allocNz;
-plint blockSize;
+plint ykTile; /* pillarTile. The assumed size, while "actual" memory layout on Length & Width are (ykTile + 2). E.g. Given computation domain 16*8*16, palabos actual allocated memory is (16+2) * (8+2) * (16+2) --> Let pillarTile=8, thus Equivalant pillar computation domain is 32 * 8 * 8. Actual allocated memory is (32+2) * (8+2) * (8+2). */
+plint NzTiles; // number of pillarTiles along Length (Z-direction)
+plint NyTiles; // number of pillarTiles along Width (Y-direction)
+
+// Using parallelepiped block Traverse
+plint blockSize; // Length of parallelogram. 
 
 // #define block_map_iX(iX, iY, iZ) (iX % ykTile + ykTile * (iZ / ykTile + (iY / ykTile) * NzTiles + (iX / ykTile) * NzTiles * NyTiles))
 
@@ -55,83 +57,87 @@ static inline uint32_t myAsmLog2(const uint32_t x) {
 }
 
 #if 1
-void allocateAndInitialize(plint Nx, plint Ny, plint Nz) {
+plint*** allocateAndInitialize(plint Nx, plint Ny, plint Nz) {
+    cout << "Palabos allocateAndInitialize\n";
+    plint allocNx = Nx + 2;
+    plint allocNy = Ny + 2;
+    plint allocNz = Nz + 2;
 
-    rawData = new plint [allocNx * allocNy * allocNz];
-    
-    // newNx = allocNx * (allocNy / ykTile) * (allocNz / ykTile);
-    newNx = allocNx * NyTiles * NzTiles;
+    palabos_rawData = new plint [allocNx * allocNy * allocNz];
+    plint* rawData = palabos_rawData;
 
-    grid  = new plint** [newNx];
-
-    plint NewNy = ykTile;
-    plint NewNz = ykTile;
-    
-    cout << newNx << ' ' << NewNy << ' ' << NewNz << '\n';
-    
-    int cnt = 0;
-    
-    for (plint iX = 0; iX < newNx; ++iX) {
-      grid[iX] = new int* [NewNy];
-      for (plint iY = 0; iY < NewNy; ++iY) {
-          grid[iX][iY] = rawData + NewNz * (iY + NewNy * iX);
-          for (plint iZ = 0; iZ < NewNz; ++ iZ) {
-              grid[iX][iY][iZ] = cnt++;
-          }
-      }        
-    }
-}
-#endif
-
-#if 0
-void allocateAndInitialize(plint Nx, plint Ny, plint Nz) {
-
-    rawData = new plint [allocNx * allocNy * allocNz];
-
-    grid  = new plint** [newNx];
+    plint*** grid  = new plint** [allocNx];
     
     int cnt = 0;   
     for (plint iX = 0; iX < allocNx; ++iX) {
       grid[iX] = new int* [allocNy];
-      for (plint iY = 0; iY < allocNz; ++iY) {
-          grid[iX][iY] = rawData + allocNz * (iY + allocNx * iX);
+      for (plint iY = 0; iY < allocNy; ++iY) {
+          grid[iX][iY] = rawData + allocNz * (iY + allocNy * iX);
           for (plint iZ = 0; iZ < allocNz; ++ iZ) {
               grid[iX][iY][iZ] = cnt++;
+              // cout << grid[iX][iY][iZ] << '\n';
           }
       }        
     }
+    cout << "Palabos allocateAndInitialize Done\n";
+    return grid;
 }
 #endif
 
-#if 0
-void orginal_access_pattern(plint Nx, plint Ny, plint Nz) {
+#if 1
+plint*** pillar_memory_allocateAndInitialize(plint Nx, plint Ny, plint Nz) {
+    cout << "pillar_memory_allocateAndInitialize\n";
+
+    plint allocNx = newNx + 2;
+    plint allocNy = ykTile + 2;
+    plint allocNz = ykTile + 2;
+    cout << newNx << ' ' << allocNy << ' ' << allocNz << '\n';
+
+    pillar_rawData = new plint [allocNx * allocNy * allocNz];
+    plint* rawData = pillar_rawData;
+
+    plint*** grid  = new plint** [allocNx];
+    
+    int cnt = 0;   
+    for (plint iX = 0; iX < allocNx; ++iX) {
+      grid[iX] = new int* [allocNy];
+      for (plint iY = 0; iY < allocNy; ++iY) {
+          grid[iX][iY] = rawData + allocNz * (iY + allocNy * iX);
+          for (plint iZ = 0; iZ < allocNz; ++ iZ) {
+            grid[iX][iY][iZ] = cnt++;
+          }
+      }        
+    }
+
+    return grid;
+}
+#endif
+
+#if 1
+void orginal_access_pattern(plint*** grid, plint Nx, plint Ny, plint Nz) {
     cout << "orginal_access_pattern\n";
 
     for (plint iX = 1; iX <= Nx; ++iX) {
+
       for (plint iY = 1; iY <= Ny; ++iY) {
+        
         for (plint iZ = 1; iZ <= Nz; ++iZ) {
 
           // Previous access patten grid[iX][iY][iZ]
+          // debug << "Org:(" << iX << ',' << iY << ',' << iZ << ")= ";
 
           // New memory map access pattern
           // 1. pillar memory layout
-          // plint iX_t = iX + allocNx * (iZ / ykTile + (iY / ykTile) * (Nz / ykTile));
-          plint iX_t = iX + allocNx * (iZ / ykTile + (iY / ykTile) * NzTiles);
+          plint iX_t = iX + Nx * ((iZ - 1) / ykTile + ((iY - 1) / ykTile) * NzTiles);
+          plint iY_t = (iY - 1) % ykTile + 1;
+          plint iZ_t = (iZ - 1) % ykTile + 1;
 
-          // 2. cube memory layout
-          // plint iX_t = iX % ykBlockH + ykTile * (iZ / ykTile + (iY / ykTile) * (Nz / ykTile) + (iX / ykBlockH)  * (Nz / ykTile)  * (ny / ykTile));
-          // plint iX_t = iX % ykBlockH + ykTile * (iZ / ykTile + (iY / ykTile) * NzTiles + (iX / ykBlockH) * NzTiles * NyTiles);
-          // plint iX_t = block_map_iX(iX, iY, iZ);
-
-          plint iY_t = iY % ykTile;
-          plint iZ_t = iZ % ykTile;
-
-          // debug << "Org:(" << iX << ',' << iY << ',' << iZ 
-          //      << ")--> New:(" << iX_t << ',' << iY_t << ',' << iZ_t << ")=";
+          // debug << "Org:(" << iX << ',' << iY << ',' << iZ
+          //     << ")--> New:(" << iX_t << ',' << iY_t << ',' << iZ_t << ")= ";
 
           int point_val = grid[iX_t][iY_t][iZ_t];
-
           debug << point_val << ' ';
+          
         }
         debug << '\n';
     }
@@ -140,8 +146,48 @@ void orginal_access_pattern(plint Nx, plint Ny, plint Nz) {
 }
 #endif
 
-void orginal_wedge_access_pattern(plint Nx, plint Ny, plint Nz) {
-  cout << "orginal_wedge_access_pattern\n";
+// Without using %, *, /
+#if 1
+void pillar_access_pattern(plint*** grid, plint Nx, plint Ny, plint Nz) {
+    cout << "pillar_access_pattern\n";
+
+    plint pillarRowTiles = Nx * NzTiles;
+
+    for (plint iX = 1; iX <= Nx; ++iX) {
+      
+      plint out_mem_iX = iX;
+      plint iY = 1;
+      for (plint tY = 0; tY < NyTiles; ++tY, out_mem_iX += pillarRowTiles) {
+
+        for (plint iiY = 1; iiY <= ykTile; ++iiY, ++iY) {
+          plint mem_iX = out_mem_iX;
+
+          plint iZ = 1;
+          for (plint tZ = 0; tZ < NzTiles; ++tZ, mem_iX += Nx) {                
+                 
+            for (plint iiZ = 1; iiZ <= ykTile; ++iiZ, ++iZ) {
+              // Previous access patten grid[iX][iY][iZ]
+
+              // Now 
+              // cout << '(' << iX << ',' << iY << ',' << iZ << ") -> "
+              //      << '[' << mem_iX << ',' << iiY << ',' << iiZ << "]; ";
+              int point_val = grid[mem_iX][iiY][iiZ];
+              debug << point_val << ' ';
+              // for inner points' neighbors grid[mem_iX+-1][iiY+-1][iiZ+-1];
+              // for outer memory points' neighbors need extra computation. 
+            }
+          }
+          debug << '\n';
+        }
+      }
+      debug << "----------------------------------------\n";
+    }
+}
+#endif
+
+void original_parallelepiped_access_pattern(plint*** grid, plint Nx, plint Ny, plint Nz) {
+  cout << "original_parallelepiped_access_pattern\n";
+
   plint XBlockID = 1;
   for (plint outerX = 1; outerX <= Nx; outerX += blockSize, XBlockID += (Nz / blockSize + 2) * (Nz / blockSize + 1)) 
   {
@@ -184,14 +230,14 @@ void orginal_wedge_access_pattern(plint Nx, plint Ny, plint Nz) {
               ++innerZ)
             {
               // Previous access patten grid[innerX][innerY][innerZ];
+
               // New memory map access pattern
               // 1. pillar memory layout
-              // plint iX_t = innerX + allocNx * (innerZ / ykTile + (innerY / ykTile) * (Nz / ykTile));
-              plint iX_t = innerX + allocNx * (innerZ / ykTile + (innerY / ykTile) * NzTiles);
-              plint iY_t = innerY % ykTile;
-              plint iZ_t = innerZ % ykTile;
+              plint iX_t = innerX + Nx * ((innerZ - 1) / ykTile + ((innerY - 1) / ykTile) * NzTiles);
+              plint iY_t = (innerY - 1) % ykTile + 1;
+              plint iZ_t = (innerZ - 1) % ykTile + 1;
 
-              debug << "Org:(" << innerX << ',' << innerY << ',' << innerZ << ") ";
+              debug << "Org:(" << innerX << ',' << innerY << ',' << innerZ << "); ";
                    // << ")--> New:(" << iX_t << ',' << iY_t << ',' << iZ_t << ")=";
 
               int point_val = grid[iX_t][iY_t][iZ_t];
@@ -207,45 +253,8 @@ void orginal_wedge_access_pattern(plint Nx, plint Ny, plint Nz) {
   }
 }
 
-// Without using %, *, /
-void pillar_access_pattern(plint Nx, plint Ny, plint Nz) {
-    cout << "pillar_access_pattern\n";
-
-    plint pillarRowTiles = allocNx * NzTiles;
-   
-    for (plint iX = 1; iX <= Nx; ++iX) {
-      
-      plint out_mem_iX = iX;
-      plint iY = 0;
-      for (plint tY = 0; tY < NyTiles; ++tY, out_mem_iX += pillarRowTiles) {
-
-        for (plint iiY = 0; iiY < ykTile; ++iiY, ++iY) {
-          if (iY == 0 || iY == Ny + 1) continue;
-
-          plint mem_iX = out_mem_iX;
-
-          plint iZ = 0;
-          for (plint tZ = 0; tZ < NzTiles; ++tZ, mem_iX += allocNx) {                
-                 
-            for (plint iiZ = 0; iiZ < ykTile; ++iiZ, ++iZ) {
-                if (iZ == 0 || iZ == Nz + 1) continue;
-                // Previous access patten grid[iX][iY][iZ]
-
-                // Now 
-                // cout << mem_iX << ',' << iiY << ',' << iiZ << "=\n";
-                int point_val = grid[mem_iX][iiY][iiZ];
-                debug << point_val << ' ';
-            }
-          }
-          debug << '\n';
-        }
-      }
-      debug << "----------------------------------------\n";
-    }
-}
-
-void pillar_wedge_access_pattern(plint Nx, plint Ny, plint Nz) {
-  cout << "orginal_access_pymd_pattern\n";
+void pillar_parallelepiped_access_pattern(plint*** grid, plint Nx, plint Ny, plint Nz) {
+  cout << "pillar_parallelepiped_access_pattern\n";
 
   for (plint outerX = 1; outerX <= Nx; outerX += blockSize) 
   {
@@ -284,20 +293,18 @@ void pillar_wedge_access_pattern(plint Nx, plint Ny, plint Nz) {
             for (plint innerZ=std::max(minZ, 1); innerZ <= std::min(maxZ, Nz);
               ++innerZ)
             {
-              // Previous access patten grid[innerX][innerY][innerZ];
               // New memory map access pattern
               // 1. pillar memory layout
-              // plint iX_t = innerX + allocNx * (innerZ / ykTile + (innerY / ykTile) * (Nz / ykTile));
-              plint iX_t = innerX + allocNx * (innerZ / ykTile + (innerY / ykTile) * NzTiles);
-              plint iY_t = innerY % ykTile;
-              plint iZ_t = innerZ % ykTile;
+              plint iX_t = innerX + Nx * ((innerZ - 1) / ykTile + ((innerY - 1) / ykTile) * NzTiles);
+              plint iY_t = (innerY - 1) % ykTile + 1;
+              plint iZ_t = (innerZ - 1) % ykTile + 1;
 
-              // debug << "Org:(" << innerX << ',' << innerY << ',' << innerZ 
-              //      << ")--> New:(" << iX_t << ',' << iY_t << ',' << iZ_t << ")=";
+              debug << "Org:(" << innerX << ',' << innerY << ',' << innerZ << "); ";
+                   // << ")--> New:(" << iX_t << ',' << iY_t << ',' << iZ_t << ")=";
 
               int point_val = grid[iX_t][iY_t][iZ_t];
 
-              debug << point_val << ' ';
+              // debug << point_val << ' ';
             }
             debug << '\n';
           }
@@ -389,11 +396,11 @@ void pillar_access_pattern2(plint Nx, plint Ny, plint Nz) {
 }
 #endif
 
-void releaseMemory(plint Nx, plint ny, plint Nz) {
-
+void releaseMemory(plint*** grid, plint* rawData, plint Nx) {
+    plint allocNx = Nx + 2;
     delete [] rawData;
-    for (plint iX = 0; iX < newNx; ++iX) {
-        delete [] grid[iX];
+    for (plint iX = 0; iX < allocNx; ++iX) {
+      delete [] grid[iX];
     }
     delete [] grid;
 }
@@ -403,48 +410,49 @@ int main(int argc, char** argv)
 {
     cout<<"Test memory map\n";
 
+    if (argc < 6) {
+      cout << "Input wrong, please recheck! Correct: ./pillar_test Nx Ny Nz ykTile blockSize\n";
+      exit(1);
+    }
+
     int Nx = atoi(argv[1]);
     int Ny = atoi(argv[2]);
     int Nz = atoi(argv[3]);
-    ykTile = atoi(argv[4]);
+    ykTile = atoi(argv[4]); // pillarTile;
     blockSize = atoi(argv[5]);
 
-    allocNx = Nx + 2;
-    allocNy = Ny + 2;
-    allocNz = Nz + 2;
-
-    if (allocNy % ykTile || allocNz % ykTile ) {
-      cout << "(Ny + 2) % ykTile != 0 || (Nz + 2) % ykTile != 0\n";
+    if (Ny % ykTile != 0 || Nz % ykTile != 0 ) {
+      cout << "Ny % ykTile != 0 || Nz % ykTile != 0\n";
       exit(1);
     }
-    NyTiles = allocNy / ykTile;
-    NzTiles = allocNz / ykTile;
+    NyTiles = Ny / ykTile;
+    NzTiles = Nz / ykTile;
+
+    // newNx = Nx * (Ny / ykTile) * (Nz / ykTile);
+    newNx = Nx * NyTiles * NzTiles;
 
     cout << "Computation domain: Nx x Ny x Nz: " << Nx << 'x' << Ny << 'x' << Nz
-         << "\nMemory allocation domain: (Nx+2) x (Ny+2) x (Nz+2): " << allocNx << 'x' << allocNy << 'x' << allocNz 
-         << "\nNzTiles=" << NzTiles << ',' << "NyTiles=" << NyTiles << '\n';
+         << "\nPalabos Memory allocation domain: (Nx+2) x (Ny+2) x (Nz+2): " << Nx + 2 << 'x' << Ny + 2 << 'x' << Nz + 2 
+         << "\npillarTile=" << ykTile << ", newNx=" << newNx << ", NzTiles=" << NzTiles << ',' << ", NyTiles=" << NyTiles 
+         << "\nPillar Memory allocation domain: (newNx+2) x (pillarTile+2) x (pillarTile+2): " << newNx + 2 << 'x' << ykTile + 2 << 'x' << ykTile + 2 << '\n';
 
     cout << (plint) log2(NzTiles) << ' ' << myLog2(NzTiles) << ' ' << myAsmLog2(NzTiles) << '\n';
     
-    allocateAndInitialize(Nx, Ny, Nz);
-    
-    #ifdef ORG    
-    orginal_access_pattern(Nx, Ny, Nz);
-    #endif
+    // allocateAndInitialize(palabos_grid, palabos_rawData, Nx, Ny, Nz);
+    palabos_grid = allocateAndInitialize(Nx, Ny, Nz);
+    pillar_grid  = pillar_memory_allocateAndInitialize(Nx, Ny, Nz);
 
-    // #ifdef ORG_PYMD
-    orginal_wedge_access_pattern(Nx, Ny, Nz);
-    // #endif
+    orginal_access_pattern(pillar_grid, Nx, Ny, Nz);
+    pillar_access_pattern(pillar_grid, Nx, Ny, Nz);
 
-    #ifdef PILLAR
-    pillar_access_pattern(Nx, Ny, Nz);
-    #endif
+    // original_parallelepiped_access_pattern(Nx, Ny, Nz);
 
     #ifdef PILLAR_PYMD
     pillar_access_pymd_pattern(Nx, Ny, Nz);
     #endif
 
-    releaseMemory(Nx, Ny, Nz);
+    releaseMemory(palabos_grid, palabos_rawData, Nx);
+    releaseMemory(pillar_grid, pillar_rawData, newNx);
     
     cout<<"Done!\n";
 
