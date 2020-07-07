eb40c13c6c8c617fac69832ddf9348e85c0e8d12
qoofyk_stampede2 <qoofyk@gmail.com>
2020-07-07 00:37:54 -0500

    try write pillar-cs-divide-threads

--- src/atomicBlock/blockLattice3D_step2_omp.hh
+++ src/atomicBlock/blockLattice3D_step2_omp.hh
@@ -1330,7 +1330,7 @@ void BlockLattice3D<T,Descriptor>::step2CollideAndStream(Box3D domain) {
 #endif // end step II
 #if 1
   // ---------------3. compute 2nd on rest surface---------------------
-  /* 2nd collide Stream on thread_boundaries, case 3.1 */
+  /* 2nd collide Stream on thread_boundaries, case 3.1 */ // can be optimized
   #pragma omp for private(iX, iY, iZ) schedule(static)
   for (iX = domain.x0 + thread_block - 1; iX <= domain.x1; iX += thread_block){
     collideRevertAndBoundSwapStream(domain, Box3D(iX, iX, domain.y0, domain.y1, domain.z0, domain.z1) );
@@ -1348,10 +1348,479 @@ void BlockLattice3D<T,Descriptor>::step2CollideAndStream(Box3D domain) {
 #endif // end _OPENMP
     // global::profiler().stop("collStream");
 }
-      #endif // end PILLAR_MEM
-
+      #endif // end if STEP2_UNROLL use PILLAR_MEM or not
     #else // NOT using STEP2_UNROLL
-    // something
+    // some implementation consider 1st and last surface but not use unrolling
+
+#if 0 // for fake testing
+template<typename T, template<typename U> class Descriptor>
+void BlockLattice3D<T,Descriptor>::step2CollideAndStream(Box3D domain) {
+  // global::profiler().start("collStream");
+  // global::profiler().increment("collStreamCells", domain.nCells());
+
+  // Make sure domain is contained within current lattice
+  PLB_PRECONDITION( contained(domain, this->getBoundingBox()) );
+
+  // For cache efficiency, memory is traversed block-wise. The three outer loops enumerate
+  //   the blocks, whereas the three inner loops enumerate the cells inside each block.
+  const plint blockSize = cachePolicy().getBlockSize();
+
+#if 0
+  printf("Here! I am step2_whole_omp_unroll_pyramid, blockSize=%ld, domain-(%ld, %ld) (%ld, %ld) (%ld, %ld)\n",
+    blockSize, domain.x0, domain.x1, domain.y0, domain.y1, domain.z0, domain.z1);
+#endif
+
+  plint iX, iY, iZ;
+#ifdef _OPENMP
+#pragma omp parallel default(shared)
+{
+  plint tid = omp_get_thread_num();
+  // printf("tid: %ld, thread_block=%ld\n", tid, thread_block);
+#if 1
+  // ---------------1. compute thread boundaries surface ---------------------
+  // 1st Collide_Revert
+  #pragma omp for private(iX, iY, iZ) schedule(static)
+  for (iX = domain.x0 + thread_block - 1; iX <= domain.x1; iX += thread_block){
+    // printf("tid: %ld, thread_block=%ld, iX=%ld, (%ld, %ld), (%ld, %ld)\n",
+    //   tid, thread_block, iX, domain.y0, domain.y1, domain.z0, domain.z1);
+    for (iY = domain.y0; iY <= domain.y1; ++iY) {
+      for (iZ = domain.z0; iZ <= domain.z1; ++iZ ){
+        grid[iX][iY][iZ].collide(this->getInternalStatistics());
+        grid[iX][iY][iZ].revert();
+      }
+    }
+  }
+#endif
+#if 1
+  // ---------------2. compute bulk ---------------------
+  #pragma omp for private(iX, iY, iZ) schedule(static, thread_block / blockSize)
+  for (plint outerX = domain.x0; outerX <= domain.x1; outerX += blockSize) {
+    // printf("tid: %ld, outerX=%ld\n", tid, outerX);
+    for (plint outerY = domain.y0; outerY <= domain.y1+blockSize-1; outerY += blockSize) {
+      // printf("outerY=%ld\n", outerY);
+      for (plint outerZ = domain.z0; outerZ <= domain.z1+2*(blockSize-1); outerZ += blockSize) {
+        // printf("outerZ=%ld\n", outerZ);
+        // Inner loops.
+        plint dx = 0;
+        for (plint innerX = outerX; innerX <= std::min(outerX+blockSize-1, domain.x1);
+          ++innerX, ++dx)
+        {
+          // Y-index is shifted in negative direction at each x-increment. to ensure
+          //   that only post-collision cells are accessed during the swap-operation
+          //   of the streaming.
+          plint minY = outerY-dx;
+          plint maxY = minY+blockSize-1;
+          plint dy = 0;
+          // printf("innerX=%ld, dx=%ld, dy=%ld, minY=%ld, maxY=%ld\n",
+          //   innerX, dx, dy, minY, maxY);
+
+          for (plint innerY=std::max(minY, domain.y0);
+            innerY <= std::min(maxY, domain.y1);
+            ++innerY, ++dy)
+          {
+            // Z-index is shifted in negative direction at each x-increment. and at each
+            //    y-increment, to ensure that only post-collision cells are accessed during
+            //    the swap-operation of the streaming.
+            plint minZ = outerZ-dx-dy;
+            plint maxZ = minZ+blockSize-1;
+            // printf("innerY=%ld, dx=%ld, dy=%ld, minY=%ld, maxY=%ld, minZ=%ld, maxZ=%ld\n",
+                          // innerY, dx, dy, minY, maxY, minZ, maxZ);
+
+            for (plint innerZ=std::max(minZ, domain.z0);
+              innerZ <= std::min(maxZ, domain.z1);
+              ++innerZ)
+            {
+              // printf("tid%ld: inner(%ld, %ld, %ld)\n", tid, innerX, innerY, innerZ);
+
+              // Other cases
+              // printf("case-4.5 inner(%ld, %ld, %ld)\n", innerX, innerY, innerZ);
+              // first Collide the cell.
+              
+              grid[innerX][innerY][innerZ].collide (
+                      this->getInternalStatistics() );
+              // Swap the populations on the cell, and then with post-collision
+              //   neighboring cell, to perform the streaming step.
+              latticeTemplates<T,Descriptor>::swapAndStream3D (
+                      grid, innerX, innerY, innerZ );
+              
+
+              // second collide
+              if (innerX > 2 && innerY > 1 && innerZ > 1 && innerY < domain.y1 && innerZ < domain.z1) {
+                grid[innerX-1][innerY-1][innerZ].collide(this->getInternalStatistics());
+                latticeTemplates<T,Descriptor>::swapAndStream3D(
+                      grid, innerX-1, innerY-1, innerZ);
+              }
+            } // end innerZ
+          } // end innerY
+        } // end innerX
+      } // end outerZ
+    } // end outerY
+  } // end outerX
+#endif // end step II
+#if 1
+  // ---------------3. compute 2nd on rest surface---------------------
+  /* 2nd collide Stream on thread_boundaries, case 3.1 */
+  #pragma omp for private(iX, iY, iZ) schedule(static)
+  for (iX = domain.x0 + thread_block - 1; iX <= domain.x1; iX += thread_block){
+    collideRevertAndBoundSwapStream(domain, Box3D(iX, iX, domain.y0, domain.y1, domain.z0, domain.z1) );
+#if 0
+    collide(Box3D(iX, iX, domain.y0, domain.y0, domain.z0, domain.z1) );
+    collide(Box3D(iX, iX, domain.y1, domain.y1, domain.z0, domain.z1) );
+    collide(Box3D(iX, iX, domain.y0 + 1, domain.y1 - 1, domain.z0, domain.z0) );
+    collide(Box3D(iX, iX, domain.y0 + 1, domain.y1 - 1, domain.z1, domain.z1) );
+
+    for (iY = domain.y0 + 1; iY < domain.y1; ++iY) {
+      for (iZ = domain.z0 + 1; iZ < domain.z1; ++iZ) {
+        grid[iX][iY][iZ].collide (this->getInternalStatistics() );
+        // swapStream(iX, iY, iZ);
+        for (plint iPop=1; iPop<=Descriptor<T>::q/2; ++iPop) {
+          plint nextX = iX + Descriptor<T>::c[iPop][0];
+          plint nextY = iY + Descriptor<T>::c[iPop][1];
+          plint nextZ = iZ + Descriptor<T>::c[iPop][2];
+
+          std::swap(grid[iX][iY][iZ][iPop+Descriptor<T>::q/2],
+                    grid[nextX][nextY][nextZ][iPop]);
+        }
+      }
+    }
+
+    boundaryStream(domain, Box3D(iX, iX, domain.y0, domain.y0, domain.z0, domain.z1) );
+    boundaryStream(domain, Box3D(iX, iX, domain.y1, domain.y1, domain.z0, domain.z1) );
+    boundaryStream(domain, Box3D(iX, iX, domain.y0 + 1, domain.y1 - 1, domain.z0, domain.z0) );
+    boundaryStream(domain, Box3D(iX, iX, domain.y0 + 1, domain.y1 - 1, domain.z1, domain.z1) );
+#endif
+  }
+
+  // 2nd swap Stream on 1st surface of every thread block, e.g. x0, x0 + thread_block, case-3.2
+  // iX = 1, 5, ... when thread_block = 4
+  #pragma omp for private(iX, iY, iZ) schedule(static)
+  for (iX = domain.x0; iX < domain.x1; iX += thread_block){
+    boundaryStream(domain, Box3D(iX, iX, domain.y0, domain.y1, domain.z0, domain.z1) );
+#if 0
+    if (iX == domain.x0)
+      boundaryStream(domain, Box3D(iX, iX, domain.y0, domain.y1, domain.z0, domain.z1) );
+    else {
+      collide(Box3D(iX, iX, domain.y0, domain.y0, domain.z0, domain.z1) );
+      collide(Box3D(iX, iX, domain.y1, domain.y1, domain.z0, domain.z1) );
+      collide(Box3D(iX, iX, domain.y0 + 1, domain.y1 - 1, domain.z0, domain.z0) );
+      collide(Box3D(iX, iX, domain.y0 + 1, domain.y1 - 1, domain.z1, domain.z1) );
+
+      for (iY = domain.y0 + 1; iY < domain.y1; ++iY) {
+        for (iZ = domain.z0 + 1; iZ < domain.z1; ++iZ) {
+          grid[iX][iY][iZ].collide (this->getInternalStatistics() );
+          // swapStream(iX, iY, iZ);
+          for (plint iPop=1; iPop<=Descriptor<T>::q/2; ++iPop) {
+            plint nextX = iX + Descriptor<T>::c[iPop][0];
+            plint nextY = iY + Descriptor<T>::c[iPop][1];
+            plint nextZ = iZ + Descriptor<T>::c[iPop][2];
+
+            std::swap(grid[iX][iY][iZ][iPop+Descriptor<T>::q/2],
+                      grid[nextX][nextY][nextZ][iPop]);
+          }
+        }
+      }
+
+      boundaryStream(domain, Box3D(iX, iX, domain.y0, domain.y0, domain.z0, domain.z1) );
+      boundaryStream(domain, Box3D(iX, iX, domain.y1, domain.y1, domain.z0, domain.z1) );
+      boundaryStream(domain, Box3D(iX, iX, domain.y0 + 1, domain.y1 - 1, domain.z0, domain.z0) );
+      boundaryStream(domain, Box3D(iX, iX, domain.y0 + 1, domain.y1 - 1, domain.z1, domain.z1) );
+    }
+#endif
+  }
+  /*-------------------------- Finish 3 -------------------------*/
+#endif // end step III
+}
+#endif // end _OPENMP
+    // global::profiler().stop("collStream");
+}
+#endif
+
+#if 1 // for fake testing pillar access pattern
+plint Ty, Tz, NyT, NzT;
+template<typename T, template<typename U> class Descriptor>
+void BlockLattice3D<T,Descriptor>::step2CollideAndStream(Box3D domain) {
+  // global::profiler().start("collStream");
+  // global::profiler().increment("collStreamCells", domain.nCells());
+
+  // Make sure domain is contained within current lattice
+  PLB_PRECONDITION( contained(domain, this->getBoundingBox()) );
+
+  // For cache efficiency, memory is traversed block-wise. The three outer loops enumerate
+  //   the blocks, whereas the three inner loops enumerate the cells inside each block.
+  const plint blockSize = cachePolicy().getBlockSize();
+
+#if 0
+  printf("Here! I am step2_whole_omp_unroll_pyramid, blockSize=%ld, domain-(%ld, %ld) (%ld, %ld) (%ld, %ld)\n",
+    blockSize, domain.x0, domain.x1, domain.y0, domain.y1, domain.z0, domain.z1);
+#endif
+
+#ifdef _OPENMP
+#pragma omp parallel default(shared)
+{
+  plint tid = omp_get_thread_num();
+  plint tid_Z = tid % Tz;
+  plint tid_Y = tid / Ty;
+  plint my_y0 = tid_Y * NyT + 1;
+  plint my_y1 = my_y0 + NyT - 1;
+  plint my_z0 = tid_Z * NzT + 1;
+  plint my_z1 = my_z0 + NzT - 1;
+  plint iX, iY, iZ;
+  // printf("tid: %ld, Y(%ld, %ld), Z(%ld, %ld)\n", tid, my_y0, my_y1, my_z0, my_z1);
+#if 1
+  // ---------------I. compute thread boundaries surface ---------------------
+  // 1st Collide_Revert
+  // front 
+  collide(Box3D(domain.x0 + 1, domain.x1 - 1,
+                  my_y0, my_y0,
+                  my_z0, my_z1) );
+  // back
+  collide(Box3D(domain.x0 + 1, domain.x1 - 1,
+                  my_y1, my_y1,
+                  my_z0, my_z1) );
+  // left
+  collide(Box3D(domain.x0 + 1,domain.x1-1,
+                my_y0 + 1, my_y1 - 1,
+                my_z0, my_z0) );
+  // right
+  collide(Box3D(domain.x0 + 1, domain.x1-1,
+                my_y0 + 1, my_y1 - 1,
+                my_z1, my_z1) );
+
+  // ---------------II compute iX = 1 bottom ---------------------
+  iX = domain.x0;
+
+  for (iY = my_y0 + 1; iY < my_y1; ++iY) {
+    for (iZ = my_z0 + 1; iZ < my_z1; ++iZ) {
+      collideRevertAndBoundSwapStream(domain, iX, iY, iZ);
+    }
+  }
+
+  // No need barrier here
+#endif
+
+
+#if 1
+  // ---------------II compute iX = 2 ---------------------
+  iX = domain.x0 + 1;
+
+  for (iY = my_y0 + 1; iY < my_y1; ++iY) {
+    for (iZ = my_z0 + 1; iZ < my_z1; ++iZ) {
+      grid[iX][iY][iZ].collide(this->getInternalStatistics());
+      latticeTemplates<T,Descriptor>::swapAndStream3D(grid, iX, iY, iZ);
+      if (iY > my_y0 + 2 && iZ > my_z0 + 2) {
+        collideRevertAndBoundSwapStream(domain, iX-1, iY-1, iZ);
+      } 
+    }
+  }
+  
+  // ---------------III compute iX = 3 ---------------------
+  iX = domain.x0 + 2;
+
+  for (iY = my_y0 + 1; iY < my_y1; ++iY) {
+    for (iZ = my_z0 + 1; iZ < my_z1; ++iZ) {
+      grid[iX][iY][iZ].collide(this->getInternalStatistics());
+      latticeTemplates<T,Descriptor>::swapAndStream3D(grid, iX, iY, iZ);
+      if (iY > my_y0 + 2 && iZ > my_z0 + 2 && iZ < my_z1 - 1) {
+        grid[iX-1][iY-1][iZ].collide(this->getInternalStatistics());
+        latticeTemplates<T,Descriptor>::swapAndStream3D(
+                      grid, iX-1, iY-1, iZ);
+      } 
+    }
+  }
+
+  // ---------------IV compute bulk ---------------------
+  for (plint outerX = domain.x0 + 3; outerX <= domain.x1 - 2; outerX += blockSize) {
+    // printf("tid: %ld, outerX=%ld\n", tid, outerX);
+    for (plint outerY = my_y0 + 1; outerY <= my_y1 - 1 + blockSize-1; outerY += blockSize) {
+      // printf("outerY=%ld\n", outerY);
+      for (plint outerZ = my_z0 + 1; outerZ <= my_z1 - 1 +2*(blockSize-1); outerZ += blockSize) {
+        // printf("outerZ=%ld\n", outerZ);
+        // Inner loops.
+        plint dx = 0;
+        for (plint innerX = outerX; innerX <= std::min(outerX+blockSize-1, domain.x1 - 2);
+          ++innerX, ++dx)
+        {
+          // Y-index is shifted in negative direction at each x-increment. to ensure
+          //   that only post-collision cells are accessed during the swap-operation
+          //   of the streaming.
+          plint minY = outerY-dx;
+          plint maxY = minY+blockSize-1;
+          plint dy = 0;
+          // printf("innerX=%ld, dx=%ld, dy=%ld, minY=%ld, maxY=%ld\n",
+          //   innerX, dx, dy, minY, maxY);
+
+          for (plint innerY=std::max(minY, my_y0 + 1);
+            innerY <= std::min(maxY, my_y1 - 1);
+            ++innerY, ++dy)
+          {
+            // Z-index is shifted in negative direction at each x-increment. and at each
+            //    y-increment, to ensure that only post-collision cells are accessed during
+            //    the swap-operation of the streaming.
+            plint minZ = outerZ-dx-dy;
+            plint maxZ = minZ+blockSize-1;
+            // printf("innerY=%ld, dx=%ld, dy=%ld, minY=%ld, maxY=%ld, minZ=%ld, maxZ=%ld\n",
+                          // innerY, dx, dy, minY, maxY, minZ, maxZ);
+
+            for (plint innerZ=std::max(minZ, my_z0 + 1);
+              innerZ <= std::min(maxZ, my_z1 - 1);
+              ++innerZ)
+            {
+              // printf("tid%ld: inner(%ld, %ld, %ld)\n", tid, innerX, innerY, innerZ);
+
+              // Other cases
+              // printf("case-4.5 inner(%ld, %ld, %ld)\n", innerX, innerY, innerZ);
+              // first Collide the cell.
+              
+              grid[innerX][innerY][innerZ].collide (
+                      this->getInternalStatistics() );
+              // Swap the populations on the cell, and then with post-collision
+              //   neighboring cell, to perform the streaming step.
+              latticeTemplates<T,Descriptor>::swapAndStream3D (
+                      grid, innerX, innerY, innerZ );
+              
+
+              // second collide
+              if (innerX > 2 && innerY > my_y0 + 2 && innerZ > my_z0 + 2 && innerZ < my_z0 - 1) {
+                grid[innerX-1][innerY-1][innerZ].collide(this->getInternalStatistics());
+                latticeTemplates<T,Descriptor>::swapAndStream3D(
+                      grid, innerX-1, innerY-1, innerZ);
+              }
+            } // end innerZ
+          } // end innerY
+        } // end innerX
+      } // end outerZ
+    } // end outerY
+  } // end outerX
+#endif // end step II
+
+  // ---------------V compute iX = top ---------------------
+  // top
+  iX = domain.x1;
+  for (iY = my_y0; iY <= my_y1; ++iY) {
+    for (iZ = my_z0; iZ <= my_z0; ++iZ) {
+      grid[iX][iY][iZ].collide(this->getInternalStatistics());
+      grid[iX][iY][iZ].revert();
+      if (iY > my_y0 + 2 && iZ > my_z0 + 2 && iZ < my_z1 - 1) {
+        grid[iX-1][iY-1][iZ].collide(this->getInternalStatistics());
+        latticeTemplates<T,Descriptor>::swapAndStream3D(
+                      grid, iX-1, iY-1, iZ);
+      } 
+    }
+  }
+
+#if 0
+  // ---------------VI. compute thread boundaries surface ---------------------
+  // 1st boundary swap stream
+  // bottom
+  bulkStream( Box3D(domain.x0, domain.x0, my_y0, my_y1, my_z0, my_z1) );
+  // top
+  bulkStream( Box3D(domain.x1, domain.x1, my_y0, my_y1, my_z0, my_z1) );
+  // front 
+  bulkStream( Box3D(domain.x0 + 1, domain.x1 - 1,
+                  my_y0, my_y0,
+                  my_z0, my_z1) );
+  // back
+  bulkStream( Box3D(domain.x0 + 1, domain.x1 - 1,
+                  my_y1, my_y1,
+                  my_z0, my_z1) );
+  // left
+  bulkStream( Box3D(domain.x0 + 1,domain.x1-1,
+                my_y0 + 1, my_y1 - 1,
+                my_z0, my_z0) );
+  // right
+  bulkStream( Box3D(domain.x0 + 1, domain.x1-1,
+                my_y0 + 1, my_y1 - 1,
+                my_z1, my_z1) );
+
+  // #pragma omp barrier
+#endif
+
+#if 0
+  // ---------------VII. compute thread boundaries surface ---------------------
+  // 1st Collide_Revert
+  // bottom
+  collide(Box3D(domain.x0, domain.x0, my_y0, my_y1, my_z0, my_z1) );
+  // top
+  collide(Box3D(domain.x1, domain.x1, my_y0, my_y1, my_z0, my_z1) );
+  // front 
+  collide(Box3D(domain.x0 + 1, domain.x1 - 1,
+                  my_y0, my_y0,
+                  my_z0, my_z1) );
+  // back
+  collide(Box3D(domain.x0 + 1, domain.x1 - 1,
+                  my_y1, my_y1,
+                  my_z0, my_z1) );
+  // left
+  collide(Box3D(domain.x0 + 1,domain.x1-1,
+                my_y0 + 1, my_y1 - 1,
+                my_z0, my_z0) );
+  // right
+  collide(Box3D(domain.x0 + 1, domain.x1-1,
+                my_y0 + 1, my_y1 - 1,
+                my_z1, my_z1) );
+
+#endif
+
+#if 0
+  // ---------------VIII, IX, X compute thread boundaries surface ---------------------
+  // VIII
+  bulkStream(Box3D(domain.x0 + 1, domain.x0 + 1, my_y0 + 1, my_y1 - 1, my_z0 + 1, my_z1 - 1));
+  // IX
+  
+  // front 
+  bulkStream(Box3D(domain.x0 + 2, domain.x1 - 2,
+                  my_y0 + 1, my_y0 + 1,
+                  my_z0 + 1, my_z1 - 1) );
+  // back
+  bulkStream(Box3D(domain.x0 + 2, domain.x1 - 2,
+                  my_y1 - 1, my_y1 - 1,
+                  my_z0 + 1, my_z1 - 1) );
+  // left
+  bulkStream(Box3D(domain.x0 + 2,domain.x1 - 2,
+                my_y0 + 1, my_y1 - 1,
+                my_z0 + 1, my_z0 + 1) );
+  // right
+  bulkStream(Box3D(domain.x0 + 2, domain.x1 - 2,
+                my_y0 + 1, my_y1 - 1,
+                my_z1 - 1, my_z1 - 1) );
+  // X
+  bulkStream(Box3D(domain.x1 - 1, domain.x1 - 1, my_y0 + 1, my_y1 - 1, my_z0 + 1, my_z1 - 1));
+  // #pragma omp barrier
+#endif  
+
+#if 0
+  // ---------------XI. compute thread boundaries surface ---------------------
+  // 2nd boundary swap stream
+  // bottom
+  bulkStream( Box3D(domain.x0, domain.x0, my_y0, my_y1, my_z0, my_z1) );
+  // top
+  bulkStream( Box3D(domain.x1, domain.x1, my_y0, my_y1, my_z0, my_z1) );
+  // front 
+  bulkStream( Box3D(domain.x0 + 1, domain.x1 - 1,
+                  my_y0, my_y0,
+                  my_z0, my_z1) );
+  // back
+  bulkStream( Box3D(domain.x0 + 1, domain.x1 - 1,
+                  my_y1, my_y1,
+                  my_z0, my_z1) );
+  // left
+  bulkStream( Box3D(domain.x0 + 1,domain.x1-1,
+                my_y0 + 1, my_y1 - 1,
+                my_z0, my_z0) );
+  // right
+  bulkStream( Box3D(domain.x0 + 1, domain.x1-1,
+                my_y0 + 1, my_y1 - 1,
+                my_z1, my_z1) );
+
+  // #pragma omp barrier
+#endif
+
+}
+#endif // end _OPENMP
+    // global::profiler().stop("collStream");
+}
+#endif
+
     #endif // end STEP2_UNROLL
 
   #endif // end STEP2_WHOLE
