d6f000c5a8c2efa452c5b3ea577256da91eda762
qoofyk <qoofyk@gmail.com>
2020-09-06 18:51:33 -0400

    update gitignore

---
+++ src/ykglobal.h
@@ -0,0 +1,16 @@
+/*
+ * Description:
+ *
+ * First created: 2020 Sep 06
+ * Last modified: 2020 Sep 06
+ *
+ * Author: Yuankun Fu
+ * e-mail: qoofyk@gmail.com
+ */
+
+#ifndef YKGLOBAL_H_
+#define YKGLOBAL_H_
+
+#define SAVE
+
+#endif
