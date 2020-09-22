20d8a7e07f2a58d072877a7684689f9feb2bbc82
qoofyk_stampede2 <qoofyk@gmail.com>
2020-09-22 11:40:38 -0500

    pass stampede2 verity

--- examples/cavity3d/verify.sh
+++ examples/cavity3d/verify.sh
@@ -47,30 +47,30 @@ for CODE in 2step_whole_prism_unroll_omp; do
   echo "---------------------------------------"
 done
 
-# echo "Verify with 2 thread"
-# export OMP_PROC_BIND=spread
-# export OMP_NUM_THREADS=2
-# for CODE in 2step_whole_prism_unroll_omp; do
-#   echo "run ${CODE}"
-#   cd ${CODE}
-#   mpirun -n 1 ./cavity3d $dim $Measure $PrismSize $Warm_up $Height $Width $Length > vel_${CODE}_${Height}_${Measure}.dat
-#   echo "verify ${CODE}"
-#   diff ../fuse/vel_fuse_${Height}_${Measure}.dat vel_${CODE}_${Height}_${Measure}.dat
-#   cd ..
-#   echo "---------------------------------------"
-# done
+echo "Verify with 2 thread"
+export OMP_PROC_BIND=spread
+export OMP_NUM_THREADS=2
+for CODE in 2step_whole_prism_unroll_omp; do
+  echo "run ${CODE}"
+  cd ${CODE}
+  mpirun -n 1 ./cavity3d $dim $Measure $PrismSize $Warm_up $Height $Width $Length > vel_${CODE}_${Height}_${Measure}.dat
+  echo "verify ${CODE}"
+  diff ../fuse/vel_fuse_${Height}_${Measure}.dat vel_${CODE}_${Height}_${Measure}.dat
+  cd ..
+  echo "---------------------------------------"
+done
 
-# echo "Verify with 4 thread"
-# export OMP_PROC_BIND=spread
-# export OMP_NUM_THREADS=4
-# for CODE in 2step_whole_prism_unroll_omp; do
-#   echo "run ${CODE}"
-#   cd ${CODE}
-#   mpirun -n 1 ./cavity3d $dim $Measure $PrismSize $Warm_up $Height $Width $Length > vel_${CODE}_${Height}_${Measure}.dat
-#   echo "verify ${CODE}"
-#   diff ../fuse/vel_fuse_${Height}_${Measure}.dat vel_${CODE}_${Height}_${Measure}.dat
-#   cd ..
-#   echo "---------------------------------------"
-# done
+echo "Verify with 4 thread"
+export OMP_PROC_BIND=spread
+export OMP_NUM_THREADS=4
+for CODE in 2step_whole_prism_unroll_omp; do
+  echo "run ${CODE}"
+  cd ${CODE}
+  mpirun -n 1 ./cavity3d $dim $Measure $PrismSize $Warm_up $Height $Width $Length > vel_${CODE}_${Height}_${Measure}.dat
+  echo "verify ${CODE}"
+  diff ../fuse/vel_fuse_${Height}_${Measure}.dat vel_${CODE}_${Height}_${Measure}.dat
+  cd ..
+  echo "---------------------------------------"
+done
 
 rm -rf */*.dat
